eeedd88a382d1f348f74b7b351d252c49e4eedff
Lars Klemstein <lrsklemstein@gmail.com>
2024-06-14 21:06:07 +0200

    implement extraction of downloadable and installed go (versions)

--- go_updater.py
+++ go_updater.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 """
-WHat's the blue light for?
+Auto updater for a local Golang installation
 
-It light's blue...
 """
 
 # bugs and hints: lrsklemstein@gmail.com
@@ -11,9 +10,18 @@ It light's blue...
 import argparse
 import logging
 import logging.config
+import os.path
+import re
+import subprocess
 import sys
 
+import requests
+from bs4 import BeautifulSoup
+
 from typing import Any, Dict  # , List, Tuple, Callable
+
+GO_DOWNLOAD_URL = 'https://go.dev/dl/'
+
 
 __log_level_default = logging.INFO
 
@@ -40,12 +48,12 @@ def get_prog_setup_or_exit_with_usage() -> Dict[str, Any]:
     log_group = parser.add_mutually_exclusive_group()
 
     parser.add_argument(
-        'FIX_ARG_EXAMPLE', help='FIX_ARG is for, well: please say it',
+        '--force', default=False,
     )
 
-#   parser.add_argument(
-#        '--optional_arg', help='an example for an optional arg',
-#   )
+    parser.add_argument(
+        'GO_PATH', help='the golang installation path',
+    )
 
     log_group.add_argument(
         '--debug', action='store_true',
@@ -93,13 +101,71 @@ def init_logging(setup: Dict[str, Any]) -> None:
 def run(setup: Dict[str, Any]) -> int:
     logger = logging.getLogger(__name__)
 
-    #
-    # this is the entry point for what your program actually does...
-    #
+    logger.info('Scan golang download page...')
+    url, version_downloadable = get_latest_go_url_and_version()
+    logger.info('Downloadable: ' + version_downloadable)
 
-    logger.info('Done something...')
+    version_installed = get_installed_go_version(setup['GO_PATH'])
+    logger.info('Installed: ' + version_installed)
+
+    if version_downloadable == version_installed:
+        logger.info('Nothing to do')
+        return 0
 
     return 0
+
+
+def get_latest_go_url_and_version() -> list[str,str]:
+    pattern_linux_href = re.compile(
+        r'/dl/(?P<version>go\d+\.\d+\.\d+)\.linux-amd64\.tar\.gz$')
+
+    session = requests.Session()
+
+    response = session.get(GO_DOWNLOAD_URL)
+
+    page = response.content.decode()
+
+    soup = BeautifulSoup(page, "html.parser")
+
+    dw = soup.find_all('a', {'class': 'download downloadBox'})
+
+    for e in dw:
+        href = e.get('href')
+        match = pattern_linux_href.match(href)
+        if match:
+            url = match.string
+            version = match.group('version')
+            return url, version
+    else:
+        raise ValueError('unable to get Linux download link')
+
+
+def get_installed_go_version(path: str) -> str:
+    """Get version of installed go, return empty string when path is absent
+    """
+    if not os.path.isdir(path):
+        return ''
+
+    go_binary = os.path.join(path, 'bin', 'go')
+
+    if not os.path.isfile(go_binary):
+        raise IOError(f'assumed go binary "{go_binary}" not found')
+
+    output = subprocess.run([go_binary, 'version'], stdout=subprocess.PIPE)
+
+    version_complete = output.stdout.decode()
+
+    version = version_complete.split()[2]
+
+    return version
+
+
+def download_go(url: str, dest_dir: str):
+    pass
+
+
+def install_go(go_archiv: str, path: str):
+    pass
 
 
 if __name__ == '__main__':
